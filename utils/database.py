a351302e5ccb61277291506da3246368ce658ac0
Charles <cweng@stuy.edu>
2017-10-24 23:29:53 -0400

    some updates

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,4 @@
-import csv, sqlite3
+import sqlite3
 
 # requested functions:
 #
@@ -11,11 +11,12 @@ c = db.cursor()
 # execute this file to create the initial database
 if __name__ == '__main__':
     # table for user login
-    c.execute("CREATE TABLE users (user TEXT, pass TEXT, PRIMARY KEY(user))" )
+    c.execute("CREATE TABLE users (user TEXT, pass TEXT, PRIMARY KEY(user))")
     # table for story ID storage; id is text because it's user created
     c.execute("CREATE TABLE stories (sID TEXT, name TEXT, PRIMARY KEY(sID))")
-    # table for update history for all stories; process this into the stories page
-    c.execute("CREATE TABLE history (sID TEXT, user TEXT, entry TEXT, PRIMARY KEY(sID, user))")
+    # table for update history for all stories
+    c.execute("CREATE TABLE history \
+                (sID TEXT, user TEXT, entry TEXT, PRIMARY KEY(sID, user))")
     # save and close database
     db.commit()
     db.close()
@@ -23,6 +24,7 @@ if __name__ == '__main__':
 # tracks if the seperate story databases need
 # initialized as false since you can only change the data with this module
 story_update = False
+
 
 # returns a dictionary for user data {user: pass}
 def getUsers():
@@ -33,18 +35,20 @@ def getUsers():
         users[line[0]] = line[1]
     return users
 
+
 # returns a dictionary for story names {sID: name}
 def getStories():
-    a = 'SELECT sID, name FROM stories
+    a = 'SELECT sID, name FROM stories'
     x = c.execute(a)
     stories = {}
     # sID is stored as TEXT so delete int cast if it happens to be an issue
     #   also add in a str cast around sID in getStory() and update despcription
     for line in x:
-        users[int(line[0])] = line[1]
-    return users
+        stories[int(line[0])] = line[1]
+    return stories
 
-# returns a list of lists (preserve order) for story entries [[user, entry],...]
+
+# returns a list of lists (in order) for story entries [[user, entry],...]
 def getStory(sID):
     name = getStories()[sID]
     a = 'SELECT user, entry FROM ' + name
@@ -53,6 +57,7 @@ def getStory(sID):
     for line in x:
         story.append([line[0], line[1]])
     return story
+
 
 # returns a list of lists for all story histories [[sID, user, entry],...]
 # not sure how else to return rows of 3 values
@@ -75,10 +80,12 @@ def insert(table, vals):
     x = x[:len(x) - 2]
     return x + ")"
 
+
 # create a table for that story
 def create(story):
     c.execute("CREATE TABLE " + story + " (user TEXT, entry TEXT, PRIMARY KEY (user))")
     db.commit()
+
 
 # add the loggin to the database
 def addUser(nick, user, password):
@@ -86,12 +93,14 @@ def addUser(nick, user, password):
     c.execute(insert("users", vals))
     db.commit()
 
+
 # creates and adds the story to the database
 def addStory(sID, story):
     create(story)
     vals = [str(sID), story]
     c.execute(insert("stories", vals))
     db.commit()
+
 
 def addUpdate(sID, user, entry):
     vals = [str(sID), user, entry]
