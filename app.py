346f0e7133e7aaa4cc704fb813188eeb2cd7a594
Arif Roktim <aroktim@stuy.edu>
2017-10-24 21:03:18 -0400

    Move auth methods to utils/auth.py

--- app.py
+++ app.py
@@ -1,51 +1,27 @@
 import sqlite3
 import os
 from flask import Flask, render_template, redirect, url_for, request, session, flash
+from utils import auth
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
-
-# hard coded user info for now
-USERS = ['bob', 'elmo']
-PASSWORDS = ['bobbins', 'goldfish']
 
 @app.route('/')
 def root():
     return render_template("base.html")
 
-def login():
-    '''
-    Logs user in.
-    '''
-    if request.form.get('username') in USERS:
-        if request.form.get('password') == PASSWORDS[USERS.index(request.form.get('username'))]:
-            session['username'] = request.form.get('username')
-            return redirect(url_for('profile'))
-        else:
-            flash("Bad password")
-            return redirect(url_for('auth'))
-    else:
-        flash("Bad username")
-        return redirect(url_for('auth'))
-
-def signup():
-    '''
-    Signs user up for the website.
-    CANNOT BE DONE UNTIL DATABASES ARE DONE
-    '''
-    return "CANNOT BE DONE UNTIL DATABASES ARE DONE<br>HARD CODED USERNAME AND PASSWORD IS 'elmo' and 'goldfish'"
 
 @app.route('/auth', methods=['GET', 'POST'])
-def auth():
+def authentication():
     # if user already logged in, redirect to profile
     if session.get('username'):
         return redirect(url_for('profile'))
     # user entered login form
     elif request.form.get('login'):
-        return login()
+        return auth.login()
     # user entered signup form
     elif request.form.get('signup'):
-        return signup()
+        return auth.signup()
     # user didn't enter form
     else:
         return render_template('auth.html')
