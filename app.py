6ebb61455b2bed68a8b2d6ef2e3c45e5e0754ec0
Shannon Lau <shannonlau00@gmail.com>
2017-10-26 13:00:03 -0400

    jen is working on shannon's comp LMAO

--- app.py
+++ app.py
@@ -45,13 +45,14 @@ def logout():
 
 @app.route('/add_story', methods=['GET', 'POST'])
 def add_story():
-     if not session.get('username'):
+    if not session.get('username'):
          flash("You must log in to add on to the story!")
-         return redirect(url_for('authentication'));
-     else:
-         content = request.form.get("content")
-         edit.add(content);
-     return render_template('add.html')
+         return redirect(url_for('authentication'))
+    elif request.form.get("update"):
+        content = request.form.get("content")
+        return edit.add(content)
+    else:
+        return render_template("add.html")
 
 if __name__ == "__main__":
     app.debug = True
